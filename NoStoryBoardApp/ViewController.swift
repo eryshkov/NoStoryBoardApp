1ef6bf3f25b26f64a125379a4e33e27c9db4aee3
Евгений Рышков <eryshkov@gmail.com>
2018-03-30 13:24:32 +0300

    Initial Commit

---
+++ NoStoryBoardApp/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  NoStoryBoardApp
+//
+//  Created by Evgeniy Ryshkov on 30.03.2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Initialize View
+        let headerView = UIView()
+        headerView.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        //Add View to the ViewController's view
+        self.view.addSubview(headerView)
+        
+        //Set position of view using constraints
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        headerView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        headerView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        headerView.centerXAnchor.constraint(equalTo: <#T##NSLayoutAnchor<NSLayoutXAxisAnchor>#>)
+        
+    }
+
+
+
+}
+
