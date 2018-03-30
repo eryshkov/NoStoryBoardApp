0de1b89cc92f62dd966991eb30d521733dea5273
Евгений Рышков <eryshkov@gmail.com>
2018-03-30 20:03:22 +0300

    Safe Area Anchors Extention

--- NoStoryBoardApp/ViewController.swift
+++ NoStoryBoardApp/ViewController.swift
@@ -10,22 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var headerView: UIView!
+    var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //Initialize View
-        let headerView = UIView()
-        headerView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        self.headerView = UIView()
+        self.headerView.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         //Add View to the ViewController's view
-        self.view.addSubview(headerView)
+        self.view.addSubview(self.headerView)
         
         //Set position of view using constraints
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-        headerView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1).isActive = true
-        headerView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1).isActive = true
-        headerView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        headerView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        self.headerView.translatesAutoresizingMaskIntoConstraints = false
+        self.headerView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1).isActive = true
+        self.headerView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1).isActive = true
+        self.headerView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.headerView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
         
+        //Initialize Label
+        titleLabel = UILabel()
+        titleLabel.text = "Numbers"
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont(name: titleLabel.font.fontName, size: 20)
+        headerView.addSubview(titleLabel)
+        
+        //Set Label's position
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.leadingAnchor.constraint(equalTo: self.safeare)
         
         
     }
